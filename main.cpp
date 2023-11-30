0059b29ddc6b9cc337a1633f085822c50097d297
0OpTiX0 <laudatijoseph@gmail.com>
2023-11-29 19:07:08 -0500

    Added a template to start from. We can change it from here if needed.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
+//For the Devs: DO NOT USE GPT! This is a demonstration of our abilities as software engineers. Lets show 
+//recruiters that we are capable of producing something functional and enjoyable!
+
+
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <random>
 
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "\nThis is a tester" << std::endl;
+    //V random seed for dice V
+    random_device rd;
+    mt19937 gen(rd());
+
+    //somewhere we can start..
+    
+    
+
+
+
     return 0;
 }
--- player.h
+++ player.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef PLAYER_H
 #define PLAYER_H
 
@@ -10,3 +11,28 @@ private:
 };
 
 #endif
+=======
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Player{
+    private:
+        string playerName;
+        double strength;
+        double perception;
+        double intelligence;
+        double endurance;
+        double luck;
+        //mechanical ability: Characters knowledge on mechanics (auto, airplane, etc..)
+        double mechAbility;
+        //Craftsmanship: Characters knowledge on how to work with their hands.
+        double cftmnsp;
+        //we'll add more from here if needed.
+
+
+
+};
+>>>>>>> 26a0cc5 (Added a template to start from. We can change it)
