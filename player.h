0326c5b3de862978620e5a9755a23aff608b7ff5
0OpTiX0 <laudatijoseph@gmail.com>
2023-11-30 15:03:54 -0500

    Added some include statements

--- player.h
+++ player.h
@@ -1,6 +1,8 @@
-<<<<<<< HEAD
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <string>
+#include <iostream>
+#include <vector>
 
 class Player{
 
@@ -11,28 +13,3 @@ private:
 };
 
 #endif
-=======
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-class Player{
-    private:
-        string playerName;
-        double strength;
-        double perception;
-        double intelligence;
-        double endurance;
-        double luck;
-        //mechanical ability: Characters knowledge on mechanics (auto, airplane, etc..)
-        double mechAbility;
-        //Craftsmanship: Characters knowledge on how to work with their hands.
-        double cftmnsp;
-        //we'll add more from here if needed.
-
-
-
-};
->>>>>>> 26a0cc5 (Added a template to start from. We can change it)
