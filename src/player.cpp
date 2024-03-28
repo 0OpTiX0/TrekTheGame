249663dff9aadaf1b28fb75546b354aebd70cd55
joe_laudati_1 <laudatijoseph@gmail.com>
2024-03-28 15:05:12 -0400

    Created a player system with an updater to change stats in the game. The player wil not be able to change them per se, but can be manipulated via in game actions. I also recreated the whole object creation method so that it's runtime complexity is O(1).

---
+++ src/player.cpp
@@ -0,0 +1,206 @@
+#include<iostream>
+#include<string>
+#include"player.h"
+
+
+
+
+//default constructor
+player::player(){
+    endurance = 0;
+    playerName ="DEFAULT";
+    intelligence = 0;
+    luck = 0;
+    strength = 0;
+}
+
+
+
+
+
+//Setters~~~~~~~~~~~
+
+
+
+//Sets playername to user selected string at beginning of the game.
+void player::setName(string &n){
+    playerName = n; 
+}
+
+//Sets player endurance at the beginning of the game.
+void player::setEnduranc(float &e){
+
+    if(e < -5 || e > 5){
+        cerr<<"ERROR: OUTSIDE STAT BOUNDS."<<endl;
+        return;
+    }
+
+    endurance = e;
+
+}
+//Sets player intelligence at the beginnign of the game.
+void player::setIntellig(float &i){
+
+    if(i < -5 || i > 5){
+        cerr<<"ERROR: OUTSIDE STAT BOUNDS."<<endl;
+        return;
+    }
+
+    intelligence = i;
+
+}
+//sets player luck at the beginning of the game
+void player::setLuck(float &l){
+
+    if(l < -5 || l > 5){
+        cerr<<"ERROR: OUTSIDE STAT BOUNDS."<<endl;
+        return;
+    }
+
+    luck = l;
+
+}
+// sets strength value at the beginning of the game.
+void player::setStrength(float &s){
+
+    if(s < -5 || s > 5){
+        cerr<<"ERROR: OUTSIDE STAT BOUNDS."<<endl;
+        return;
+    }
+
+    strength = s;
+}
+
+
+//changes player stat values in game
+void player::changeStat(string &t, float &cs){
+    //base cases to prevent bounding errors.
+    if (getLuck()>5 || getLuck()<-5){
+        cout<<"LV MAX"<<endl;
+        return;
+    }
+
+    else if(getIntellig()>5 || getIntellig()<-5){
+        cout<<"LV MAX"<<endl;
+        return;
+    }
+
+    else if(getStrength()>5 || getStrength()<-5){
+        cout<<"LV MAX"<<endl;
+        return;
+    }
+      
+    else if(getEnduranc()>5 || getEnduranc()<-5){
+        cout<<"LV MAX"<<endl;
+        return;
+    }
+    //if lv is not maxed, lv is changed. IF lv change goes over boundary, the value is set to max value.
+    else{
+
+        if(t == "lukUp"){
+            luck = luck + cs;
+            if(luck > 5){
+                luck = 5;
+            }
+
+        }
+
+        if(t == "lukDn"){
+            luck = luck - cs;
+            if(luck < -5){
+                luck = -5;
+            }
+
+        }
+
+
+
+
+        if(t == "strUp"){
+            strength = strength + cs;
+            if(strength > 5){
+                strength = 5;
+            }
+
+        }
+
+        if(t == "strDn"){
+            strength = strength - cs;
+            if(strength< -5){
+                strength = -5;
+            }
+
+        }
+
+
+
+        if(t == "endUp"){
+            endurance = endurance + cs;
+            if(endurance > 5){
+                endurance = 5;
+            }
+
+        }
+
+        if(t == "endDn"){
+            endurance = endurance - cs;
+            if(endurance < -5){
+                endurance = -5;
+            }
+
+        }
+
+
+
+        if(t == "intUp"){
+            intelligence = intelligence + cs;
+            if(intelligence > 5){
+                intelligence = 5;
+            }
+        }
+
+        if(t == "intDn"){
+            intelligence = intelligence - cs;
+            if(intelligence < -5){
+                intelligence = -5;
+            }
+        }
+    }
+}
+
+
+
+
+
+//Getters~~~~~~~~~~~~
+
+
+
+
+//Sends playername elsewhere
+string player::getName(){
+    return playerName;
+}
+//sends strength value elsewhere
+float player::getStrength(){
+    return strength;
+}
+
+//sends intelligence value elsewhere
+float player::getIntellig(){
+    return intelligence;
+}
+
+//sends endurance value elsewhere
+float player::getEnduranc(){
+    return endurance;
+}
+
+//sends luck value elsewhere
+float player::getLuck(){
+    return luck;
+}
+
+
+
+
--- src/player.h
+++ src/player.h
@@ -11,21 +11,22 @@ using namespace std;
 //This Class is to manage the player character for the game. 
 //It will hold all stats that will be used for skill checks.
 
-template <class T>
 class player{
     private:
-        string playerName = 0;
-        float luck = 0;
-        float strength = 0;
-        float intelligence = 0;
-        float endurance = 0;
+        string playerName;
+        float luck;
+        float strength;
+        float intelligence;
+        float endurance;
     public:
         player();
         void setName(string &n);
         void setLuck(float &l);
         void setStrength(float &s);
         void setIntellig(float &i);
-        void setEnduranc(foat &e);
+        void setEnduranc(float &e);
+        void changeStat(string &t, float &cs);
+        
 
         string getName();
         float getLuck();
